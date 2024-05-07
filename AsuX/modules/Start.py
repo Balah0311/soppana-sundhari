b9b2f57d00994ec843d76a2050646aefd86075cd
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-07 20:59:41 +0530

    Update Start.py

--- AsuX/modules/Start.py
+++ AsuX/modules/Start.py
@@ -11,16 +11,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     akboss.append(
         [
             InlineKeyboardButton(
-                text="𝐍𝐚 𝐏𝐚𝐯𝐚𝐦'𝐥𝐚 𝐀𝐝𝐝 𝐏𝐚𝐧𝐧𝐢𝐤𝐨",
+                text="𝐀ᴅᴅ 𝐌ᴇ",
                 url=f"http://t.me/{context.bot.username}?startgroup=true"),
             InlineKeyboardButton(
-                text="𝐃𝐚𝐫𝐥𝐢𝐧𝐠",
-                url=f"https://t.me/Idhayann"),
+                text="𝐍ᴇᴛᴡᴏʀᴋ",
+                url=f"https://t.me/team_hypers_networks"),
             
         ]
     )
     await msg.reply_text(
-        f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n",
+        f"╭──────⌁᳀⌁╾─────╮\n ‎ ‎ ‎ ‎ [𝐓ᴇᴀᴍ 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ](https://t.me/Team_Hypers_Networks)\n╰─────╼⌁᳀⌁╾─────╯</b>\n\n<b>𝐔ɴᴅᴇʀ 𝐇ʏᴘᴇʀ 𝐂ᴏɴᴛʀᴏʟ {context.bot.first_name}</b>\n\n<b>𝐀ɴʏ 𝐈ssᴜ 𝐓ᴏ 𝐂ᴏɴᴛᴀᴄᴛ 𝐓O 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ - [𝐓ᴇᴀᴍ 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ](https://t.me/Team_Hypers_Networks) </b>\n\n<b>",
         reply_markup=InlineKeyboardMarkup(akboss),
     )
 
@@ -32,21 +32,21 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     akboss.append(
         [
             InlineKeyboardButton(
-                text="𝐍𝐚 𝐏𝐚𝐯𝐚𝐦'𝐥𝐚 𝐀𝐝𝐝 𝐏𝐚𝐧𝐧𝐢𝐤𝐨",
+                text="𝐀ᴅᴅ 𝐌ᴇ",
                 url=f"http://t.me/{context.bot.username}?startgroup=true"),
             InlineKeyboardButton(
-                text="𝐃𝐚𝐫𝐥𝐢𝐧𝐠",
-                url=f"https://t.me/Idhayann"),
+                text="𝐍ᴇᴛᴡᴏʀᴋ",
+                url=f"https://t.me/team_hypers_networks"),
         ]
     )
     await msg.reply_text(
-        f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n\nɪ ᴄᴀɴ ʀᴇꜱᴛʀɪᴄᴛ ᴡʜɪᴄʜ ᴄᴏɴᴛᴀɪɴꜱ ᴘᴜʙʟɪᴄ ᴄʜᴀᴛ ᴜꜱᴇʀɴᴀᴍᴇ ᴍᴇꜱꜱᴀɢᴇꜱ",
+        f"╭──────⌁᳀⌁╾─────╮\n ‎ ‎ ‎ ‎ [𝐓ᴇᴀᴍ 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ](https://t.me/Team_Hypers_Networks)\n╰─────╼⌁᳀⌁╾─────╯</b>\n\n<b>𝐔ɴᴅᴇʀ 𝐇ʏᴘᴇʀ 𝐂ᴏɴᴛʀᴏʟ {context.bot.first_name}</b>\n\n<b>𝐀ɴʏ 𝐈ssᴜ 𝐓ᴏ 𝐂ᴏɴᴛᴀᴄᴛ 𝐓O 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ - [𝐓ᴇᴀᴍ 𝐇ʏᴘᴇʀ 𝐍ᴇᴛᴡᴏʀᴋ](https://t.me/Team_Hypers_Networks) </b>\n\n<b>",",
         reply_markup=InlineKeyboardMarkup(akboss),
     )
 
 
-START = CommandHandler(["chatbot", "start"], start, block=False)
-HELP = CommandHandler(["chelp", "help"], help, block=False)
+START = CommandHandler(["chatbot", "start"], start, block=True)
+HELP = CommandHandler(["chelp", "help"], help, block=True)
 
 rani.add_handler(START)
 rani.add_handler(HELP)
